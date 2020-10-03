92432e1b7f7f070f2f6450e3333c09495fd73827
mistip83 <65766909+mistip83@users.noreply.github.com>
2020-10-03 13:05:29 +0100

    Added BlogPostsController

---
+++ Istip.Blog.API/Controllers/BlogPostsController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Istip.Blog.API.DataAccess;
+using Istip.Blog.API.Models;
+
+namespace Istip.Blog.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly BlogPostsContext _context;
+
+        public BlogPostsController(BlogPostsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BlogPosts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BlogPost>>> GetBlogPost()
+        {
+            return await _context.BlogPost.ToListAsync();
+        }
+
+        // GET: api/BlogPosts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BlogPost>> GetBlogPost(int id)
+        {
+            var blogPost = await _context.BlogPost.FindAsync(id);
+
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return blogPost;
+        }
+
+        // PUT: api/BlogPosts/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBlogPost(int id, BlogPost blogPost)
+        {
+            if (id != blogPost.PostId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(blogPost).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BlogPostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BlogPosts
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<BlogPost>> PostBlogPost(BlogPost blogPost)
+        {
+            _context.BlogPost.Add(blogPost);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBlogPost", new { id = blogPost.PostId }, blogPost);
+        }
+
+        // DELETE: api/BlogPosts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BlogPost>> DeleteBlogPost(int id)
+        {
+            var blogPost = await _context.BlogPost.FindAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            _context.BlogPost.Remove(blogPost);
+            await _context.SaveChangesAsync();
+
+            return blogPost;
+        }
+
+        private bool BlogPostExists(int id)
+        {
+            return _context.BlogPost.Any(e => e.PostId == id);
+        }
+    }
+}
---
+++ Istip.Blog.API/Data/BlogPostsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Istip.Blog.API.DataAccess
+{
+    public class BlogPostsContext : DbContext
+    {
+        public BlogPostsContext (DbContextOptions<BlogPostsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.BlogPost> BlogPost { get; set; }
+    }
+}
