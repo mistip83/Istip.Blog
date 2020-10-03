eed0ee274c23d3d13c4dbb557a1ad52adc6f6306
mistip83 <65766909+mistip83@users.noreply.github.com>
2020-10-03 21:11:52 +0100

    Update BlogPostsController.cs

--- Istip.Blog.API/Controllers/BlogPostsController.cs
+++ Istip.Blog.API/Controllers/BlogPostsController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Istip.Blog.API.DataAccess;
 using Istip.Blog.API.Models;
+using Istip.Blog.API.Data;
 
 namespace Istip.Blog.API.Controllers
 {
@@ -16,23 +15,30 @@ namespace Istip.Blog.API.Controllers
     public class BlogPostsController : ControllerBase
     {
         private readonly BlogPostsContext _context;
+        private readonly IDataRepository<BlogPost> _repo;
 
-        public BlogPostsController(BlogPostsContext context)
+        public BlogPostsController(BlogPostsContext context, IDataRepository<BlogPost> repo)
         {
             _context = context;
+            _repo = repo;
         }
 
         // GET: api/BlogPosts
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<BlogPost>>> GetBlogPost()
+        public IEnumerable<BlogPost> GetBlogPosts()
         {
-            return await _context.BlogPost.ToListAsync();
+            return _context.BlogPost.OrderByDescending(p => p.PostId);
         }
 
         // GET: api/BlogPosts/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<BlogPost>> GetBlogPost(int id)
+        public async Task<IActionResult> GetBlogPost([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             var blogPost = await _context.BlogPost.FindAsync(id);
 
             if (blogPost == null)
@@ -40,15 +46,20 @@ namespace Istip.Blog.API.Controllers
                 return NotFound();
             }
 
-            return blogPost;
+            return Ok(blogPost);
         }
 
         // PUT: api/BlogPosts/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutBlogPost(int id, BlogPost blogPost)
+        public async Task<IActionResult> PutBlogPost([FromRoute] int id, [FromBody] BlogPost blogPost)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             if (id != blogPost.PostId)
             {
                 return BadRequest();
@@ -58,7 +69,8 @@ namespace Istip.Blog.API.Controllers
 
             try
             {
-                await _context.SaveChangesAsync();
+                _repo.Update(blogPost);
+                var save = await _repo.SaveAsync(blogPost);
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -79,28 +91,38 @@ namespace Istip.Blog.API.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<BlogPost>> PostBlogPost(BlogPost blogPost)
+        public async Task<IActionResult> PostBlogPost([FromBody] BlogPost blogPost)
         {
-            _context.BlogPost.Add(blogPost);
-            await _context.SaveChangesAsync();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _repo.Add(blogPost);
+            var save = await _repo.SaveAsync(blogPost);
 
             return CreatedAtAction("GetBlogPost", new { id = blogPost.PostId }, blogPost);
         }
 
         // DELETE: api/BlogPosts/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<BlogPost>> DeleteBlogPost(int id)
+        public async Task<IActionResult> DeleteBlogPost([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             var blogPost = await _context.BlogPost.FindAsync(id);
             if (blogPost == null)
             {
                 return NotFound();
             }
 
-            _context.BlogPost.Remove(blogPost);
-            await _context.SaveChangesAsync();
+            _repo.Delete(blogPost);
+            var save = await _repo.SaveAsync(blogPost);
 
-            return blogPost;
+            return Ok(blogPost);
         }
 
         private bool BlogPostExists(int id)
