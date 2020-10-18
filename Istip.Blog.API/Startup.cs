c10151c99aab99544d2cefc2ca4c442ae928795a
mistip83 <65766909+mistip83@users.noreply.github.com>
2020-10-18 11:31:02 +0100

    Update Program.cs and Startup.cs

--- Istip.Blog.API/Startup.cs
+++ Istip.Blog.API/Startup.cs
@@ -3,10 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Istip.Blog.API.DataAccess;
-using Istip.Blog.API.Data;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
+
 
 namespace Istip.Blog.API
 {
@@ -19,70 +16,41 @@ namespace Istip.Blog.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            services.AddDbContext<BlogPostsContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("BlogPostsContext")));
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy",
-                    builder => builder.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
-                        //.AllowCredentials());
+            services.AddSpaStaticFiles(configuration => {
+                configuration.RootPath = @"C:\\Users\\m\\source\\repos\\Istip.Blog\\ClientApp\\dist";
             });
 
-            services.AddScoped(typeof(IDataRepository<>), typeof(DataRepository<>));
-
-            services.AddMvc(x => x.EnableEndpointRouting = false);
-
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseCors("CorsPolicy");
+            app.UseCors(builder => builder
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .SetIsOriginAllowed((host) => true)
+              .AllowCredentials()
+              );
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
 
-            app.UseMvc(routes =>
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
 
+            app.UseSpaStaticFiles();
             app.UseSpa(spa =>
             {
-                // to learn more about options for serving an angular spa from asp.net core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "clientapp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
+                spa.Options.SourcePath = @"C:\\Users\\m\\source\\repos\\Istip.Blog\\ClientApp";
             });
         }
     }
